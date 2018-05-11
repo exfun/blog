b41050c52acc06aedfc05f68c8ea2861c559136c
lanten <974986907@qq.com>
2018-05-11 20:46:24 +0800

    build

--- build/build.js
+++ build/build.js
@@ -3,6 +3,35 @@ import path from 'path'
 import webpack from 'webpack'
 import chalk from 'chalk'
 
+import webpackConfig from '../config/webpack.config'
+
 import { clearDir } from './utils'
 
 clearDir(path.join(__dirname, '../dist'))
+
+webpack(webpackConfig, (err, stats) => {
+  if (err) {
+    throw err
+  }
+  console.log(chalk.yellow('start building ...'))
+  process.stdout.write(
+    stats.toString({
+      colors: true,
+      hash: false,
+      version: true,
+      timings: true,
+      assets: true,
+      chunks: false,
+      children: false,
+      modules: false,
+    }) + '\n\n'
+  )
+
+  if (stats.hasErrors() || stats.hasWarnings()) {
+    console.log(chalk.red('building failed'))
+    return
+  }
+
+  console.log(chalk.greenBright('building successfully'))
+  return
+})
